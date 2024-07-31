eecac3bead61710d8b102eda3e84d913dba7a415
opeyemi-jonah <gavrieljonah@gmail.com>
2024-07-31 14:41:25 -0600

    Reformat class construct

--- models/Customer.py
+++ models/Customer.py
@@ -1,34 +1,39 @@
 class Customers:
-    def __init__(self, customer_id=0, name="", contact=("","")):
+    def __init__(self, customer_id=0, name="", contact=("", "")):
         self.__customer_id = customer_id
         self.__name = name
         self.__contact = contact
-    
-    # Customer ID Mutator & Accessor
+
+    # Accessor methods (getters)
     def get_customer_id(self):
         return self.__customer_id
-    
-    def set_customer_id(self, customer_id):
-        self.__customer_id = customer_id
-    
-    customer_id = property(get_customer_id, set_customer_id)
-    
-    # Name Mutator & Accessor
+
     def get_name(self):
         return self.__name
-    
+
+    def get_contact(self):
+        return self.__contact
+
+    # Mutator methods (setters)
+    def set_customer_id(self, customer_id):
+        self.__customer_id = customer_id
+
     def set_name(self, name):
         self.__name = name
 
-    name = property(get_name, set_name)
-    
-    
-    # Contact Mutator & Accessor
-    def get_contact(self):
-        return self.__contact
-    
     def set_contact(self, contact):
         self.__contact = contact
 
+    # Properties for accessors and mutators
+    customer_id = property(get_customer_id, set_customer_id)
+    name = property(get_name, set_name)
     contact = property(get_contact, set_contact)
-    
+
+# Usage example
+customer = Customers(1, "John Doe", ("john@example.com", "123-456-7890"))
+print(customer.get_customer_id())  # 1
+print(customer.get_name())  # John Doe
+print(customer.get_contact())  # ('john@example.com', '123-456-7890')
+
+customer.set_name("Jane Doe")
+print(customer.get_name())  # Jane Doe
